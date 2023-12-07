95b7b829fda58638b23485843ae6ebf934b469fd
Scarlett Archer <scarlett_archer6@yahoo.com>
2023-12-07 12:06:04 +0000

    Body (#1)
    * Add CSS to nav and header of index.html
    
    * Add CSS to index.html and projects.html
    
    * remove this
    
    * Add responsive design
    
    * Additional JavaScript for the nav bar
    
    * Final touches to CSS- responsive design
    
    * Final additions of JavaScript and relevant comments
    
    * Final additons of JavaScript and relevant comments
    
    * Final touches of CSS- responsive design and JavaScript, as well as relevant comments
    
    * Additions of CSS-responsive design, JavaScript and HTML

---
+++ resources/script/script.js
@@ -0,0 +1,54 @@
+const randButton = document.querySelector("button");
+
+const randomFacts = [
+  "I played football at Academy level for WestHam, Leyton Orient and Colchester⚽️",
+  "I have a pet Cockerspaniel called Winnie🐶",
+  "I swam for Redbridge county🏊‍♀️",
+  "I played football for Hertfordshire County⚽️",
+];
+
+function randFunc() {
+  randButton.innerHTML = randomFact(randomFacts);
+}
+
+const randomFact = (array) => {
+  let random = Math.floor(Math.random() * randomFacts.length);
+  let phrase = array[random];
+  return phrase;
+
+};
+
+randButton.onclick = randFunc;
+
+//-------------------Function for nav bar depending on width of screen -----------------
+//When in mobile view, only the h1 appears until mouse enters then the nav bar appears. Once it leaves, it disappears again.
+
+/*const navBar = document.querySelector('h1');
+const navMenu = document.querySelector('ul');
+
+//connecting it to media query
+
+function navFunction(width) {
+    if (width.matches) {
+        navBar.onmouseenter = function() {
+            navMenu.style.display = "block"
+        }
+
+        //hiding nav bar
+        navMenu.onmouseleave = function() {
+            navMenu.style.display = 'none';
+        };
+    } else {
+            navMenu.style.display = '';  
+}}
+
+//create media query list object
+let width = window.matchMedia('(max-width: 500px)');
+
+//call listener function
+navFunction(width);
+
+//attach listener function on state changes
+width.addEventListener("change", function() {
+    navFunction(width);
+});*/
