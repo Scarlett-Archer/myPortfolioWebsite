f15aab57d91a02d6a5906b23bc6a406fd5d0698e
Scarlett Archer <scarlett_archer6@yahoo.com>
2023-12-07 10:34:58 +0000

    Final additions of JavaScript and relevant comments

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,4 +1,5 @@
 const randButton = document.querySelector("button");
+
 const randomFacts = [
   "I played football at Academy level for WestHam, Leyton Orient and Colchester⚽️",
   "I have a pet Cockerspaniel called Winnie🐶",
@@ -7,7 +8,6 @@ const randomFacts = [
 ];
 
 function randFunc() {
-  //document.getElementsByClassName("randomFacts").innerHTML = "HELLO";
   randButton.innerHTML = randomFact(randomFacts);
 }
 
@@ -16,7 +16,41 @@ const randomFact = (array) => {
   let phrase = array[random];
   return phrase;
 
-  
 };
 
 randButton.onclick = randFunc;
+
+
+
+//-------------------Function for nav bar depending on width of screen --------------
+//When in mobile view, only the h1 appears until mouse enters then the nav bar appears. Once it leaves, it disappears again.
+
+/*const navBar = document.querySelector('h1');
+const navMenu = document.querySelector('ul');
+
+//connecting it to media query
+
+function navFunction(width) {
+    if (width.matches) {
+        navBar.onmouseenter = function() {
+            navMenu.style.display = "block"
+        }
+
+        //hiding nav bar
+        navMenu.onmouseleave = function() {
+            navMenu.style.display = 'none';
+        };
+    } else {
+            navMenu.style.display = '';  
+}}
+
+//create media query list object
+let width = window.matchMedia('(max-width: 500px)');
+
+//call listener function
+navFunction(width);
+
+//attach listener function on state changes
+width.addEventListener("change", function() {
+    navFunction(width);
+});*/
